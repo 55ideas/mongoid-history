1bd3a92b6312f68eb7f578da69d7818714574b5e
Gleb Tv <glebtv@gmail.com>
2012-10-09 08:12:05 +0400

    Revert comments

--- lib/mongoid/history/sweeper.rb
+++ lib/mongoid/history/sweeper.rb
@@ -8,11 +8,19 @@ module Mongoid::History
       Thread.current[:mongoid_history_sweeper_controller] = value
     end
 
+    # Hook to ActionController::Base#around_filter.
+    # Runs before a controller action is run.
+    # It should always return true so controller actions
+    # can continue.
     def before(controller)
       self.controller = controller
       true
     end
 
+    # Hook to ActionController::Base#around_filter.
+    # Runs after a controller action is run.
+    # Clean up so that the controller can
+    # be collected after this request
     def after(controller)
       self.controller = nil
     end
